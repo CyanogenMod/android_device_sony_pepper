6924f669358832c6aee09e5c4a94d5248f7bec4b
freexperia <jerpelea@gmail.com>
2012-10-31 17:57:56 +0200

    pepper: add and enable NFC
    Change-Id: Ief392264010b7196323d7a3864b845d0674cfca9

--- pepper.mk
+++ pepper.mk
@@ -87,6 +87,15 @@ PRODUCT_PACKAGES += \
     Nfc \
     Tag
 
+# NFCEE access control
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    NFCEE_ACCESS_PATH := device/sony/montblanc-common/config/nfcee_access.xml
+else
+    NFCEE_ACCESS_PATH := device/sony/montblanc-common/config/nfcee_access_debug.xml
+endif
+PRODUCT_COPY_FILES += \
+    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
+
 $(call inherit-product, frameworks/base/build/phone-hdpi-512-dalvik-heap.mk)
 
 $(call inherit-product-if-exists, vendor/sony/pepper/pepper-vendor.mk)
